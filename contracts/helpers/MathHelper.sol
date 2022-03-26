7dbb383e7f26009aed15feb13248d6215f46beba
2xic <5640782+2xic@users.noreply.github.com>
2022-03-26 15:39:18 +0100

    More work o nthe rebalance logic, updated supply adjustment

--- contracts/helpers/MathHelper.sol
+++ contracts/helpers/MathHelper.sol
@@ -1,48 +1,40 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import 'hardhat/console.sol';
+
 library MathHelper {
-    uint256 private constant EXPONENT = 1000;
-    uint256 private constant PERCENTAGE_SCALE = 100;
+	uint256 private constant EXPONENT = 1000;
+	uint256 private constant PERCENTAGE_SCALE = 100;
 
-    function relativeDivide(uint256 a, uint256 b)
-        public
-        pure
-        returns (uint256)
-    {
-        if (b < a) {
-            return
-                (a *
-                    EXPONENT *
-                    PERCENTAGE_SCALE -
-                    b *
-                    EXPONENT *
-                    PERCENTAGE_SCALE) / (b * EXPONENT);
-        } else {
-            return
-                (b *
-                    EXPONENT *
-                    PERCENTAGE_SCALE -
-                    a *
-                    EXPONENT *
-                    PERCENTAGE_SCALE) / (a * EXPONENT);
-        }
-    }
+	function relativeDivide(
+		uint256 a,
+		uint256 b,
+		uint256 c
+	) public pure returns (uint256) {
+		return
+			(a *
+				EXPONENT *
+				PERCENTAGE_SCALE -
+				b *
+				EXPONENT *
+				PERCENTAGE_SCALE) / (c * EXPONENT);
+	}
 
-    function multiplyPercentage(uint256 number, uint256 scaledPercentage)
-        public
-        pure
-        returns (uint256)
-    {
-        return
-            (number * (PERCENTAGE_SCALE + scaledPercentage)) / PERCENTAGE_SCALE;
-    }
+	function multiplyPercentage(uint256 number, uint256 scaledPercentage)
+		public
+		pure
+		returns (uint256)
+	{
+		return
+			(number * (PERCENTAGE_SCALE + scaledPercentage)) / PERCENTAGE_SCALE;
+	}
 
-    function increasePrecision(uint256 number) public pure returns (uint256) {
-        return number * EXPONENT;
-    }
+	function increasePrecision(uint256 number) public pure returns (uint256) {
+		return number * EXPONENT;
+	}
 
-    function normalizeNumber(uint256 number) public pure returns (uint256) {
-        return number / EXPONENT;
-    }
+	function normalizeNumber(uint256 number) public pure returns (uint256) {
+		return number / EXPONENT;
+	}
 }
--- contracts/helpers/SimpleRebalanceHelper.sol
+++ contracts/helpers/SimpleRebalanceHelper.sol
@@ -1,34 +1,48 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import {SharedStructs} from "../structs/SharedStructs.sol";
-import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SharedStructs} from "../structs/SharedStructs.sol";
-import {MathHelper} from "./MathHelper.sol";
+import {SharedStructs} from '../structs/SharedStructs.sol';
+import 'hardhat/console.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {SharedStructs} from '../structs/SharedStructs.sol';
+import {MathHelper} from './MathHelper.sol';
 
 library SimpleRebalanceHelper {
-    using MathHelper for uint256;
+	using MathHelper for uint256;
 
-    function rebalancePools(
-        uint256 price,
-        SharedStructs.PoolState storage poolState
-    ) public returns (SharedStructs.PoolState memory) {
-        if (poolState.price < price) {
-            uint256 relativePriceChange = MathHelper.relativeDivide(
-                poolState.price,
-                price
-            );
+	function rebalancePools(
+		uint256 price,
+		SharedStructs.PoolState storage poolState
+	) public returns (SharedStructs.PoolState memory) {
+		if (poolState.price < price) {
+			uint256 relativePriceChange = MathHelper.relativeDivide(
+				price,
+				poolState.price,
+				poolState.price
+			);
+			uint256 poolAdjustment = MathHelper.multiplyPercentage(
+				poolState.longPoolSize,
+				relativePriceChange
+			) - poolState.longPoolSize;
 
-            uint256 poolAdjustment = MathHelper.multiplyPercentage(
-                poolState.longPoolSize,
-                relativePriceChange
-            ) - poolState.longPoolSize;
+			poolState.longPoolSize += poolAdjustment;
+			poolState.shortPoolSize -= poolAdjustment;
+		} else {
+			uint256 relativePriceChange = MathHelper.relativeDivide(
+				poolState.price,
+				price,
+				poolState.price
+			);
 
-            poolState.longPoolSize += poolAdjustment;
-            poolState.shortPoolSize -= poolAdjustment;
-        }
+			uint256 poolAdjustment = MathHelper.multiplyPercentage(
+				poolState.shortPoolSize,
+				relativePriceChange
+			) - poolState.shortPoolSize;
 
-        return poolState;
-    }
+			poolState.shortPoolSize += poolAdjustment;
+			poolState.longPoolSize -= poolAdjustment;
+		}
+
+		return poolState;
+	}
 }
